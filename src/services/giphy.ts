d305710ace8e52408fd35d6ecdc2b3c3b785f57d
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-07-25 22:48:32 -0400

    ✨ Use the translate endpoint for giphy search

--- src/services/giphy.ts
+++ src/services/giphy.ts
@@ -17,31 +17,21 @@ interface Gif {
   embed_url: string
 }
 
-interface GiphyResponse {
-  data: Gif[]
-}
-
 // Function to get a random Giphy link based on a search term
 export async function getRandomGiphyLink(searchTerm: string): Promise<string> {
   try {
-    const response = await axios.get<GiphyResponse>(
-      "https://api.giphy.com/v1/gifs/search",
+    const response = await axios.get<{ data: Gif }>(
+      "https://api.giphy.com/v1/gifs/translate",
       {
         params: {
           api_key: giphyApiKey,
-          q: searchTerm,
-          limit: 50, // You can adjust the limit based on your preference
+          s: searchTerm,
           rating: "G", // You can adjust the rating if needed
         },
       },
     )
-    const gifs = response.data.data
-    if (gifs.length === 0) {
-      throw new Error("No GIFs found for the search term")
-    }
-    // Select a random GIF from the results
-    const randomGif = gifs[Math.floor(Math.random() * gifs.length)]
-    return randomGif.url
+    const gif = response.data.data
+    return gif.url
   } catch (error) {
     console.error("Error fetching Giphy link: ", error)
     throw error
